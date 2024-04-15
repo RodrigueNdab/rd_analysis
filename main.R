c8d531433ffe36df086e1ec41b8ea7f3c5314c52
Rodrigue Ndabashinze <ndabrodriguez@gmail.com>
2024-04-15 20:07:00 +0200

    filtered to 30 and above

---
+++ main.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+tv_hours_table <- gss_cat %>%
+  filter(age >= 30)  %>% group_by(marital) %>% 
+   summarize(mean_tv_hours = mean(tvhours,  na.rm =T))
+write_csv(tv_hours_table, here("TV_hours_by_marital.csv"))
